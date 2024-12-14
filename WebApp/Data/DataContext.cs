10b8cef25d0e2fcca73c77cb14eaa9f9d1bcaf29
GAMASU Rangha Vardhan <rangha.vardhan.gamasu@hexagon.com>
2024-12-14 18:26:06 +0530

    asp.net app commit

---
+++ WebApp/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Entities;
+namespace WebApp.Data
+
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users{ get; set; }
+    }
+    
+    
+}
